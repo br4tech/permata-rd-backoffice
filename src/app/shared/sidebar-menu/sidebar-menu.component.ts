553c128633b49c65eea9360e63e10d5109cf0cb4
Guilherme Silva <guilherme.tr.silva@gmail.com>
2019-03-02 16:51:40 -0300

    Alteração do layout

--- src/app/shared/sidebar-menu/sidebar-menu.component.ts
+++ src/app/shared/sidebar-menu/sidebar-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-sidebar-menu',
@@ -10,6 +11,17 @@ export class SidebarMenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+      $('.list-group').on('show', function(e) {
+      $(this).find('.collapse').removeClass('arrow-down').addClass('arrow-up');
+      e.stopPropagation();
+      });
+      $('.list-group').on('hide', function(e) {
+      $(this).find('.collapse').removeClass('arrow-up').addClass('arrow-down');
+      e.stopPropagation();
+  });
   }
+
+
 
 }
