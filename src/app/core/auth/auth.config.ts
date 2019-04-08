550ef7acdc3d694655dd464b63fb7c0bceb283cb
Guilherme Silva <guilherme.tr.silva@gmail.com>
2019-04-08 20:47:48 -0300

    Alteração no arquivo de autorização

--- src/app/core/auth/auth.config.ts
+++ src/app/core/auth/auth.config.ts
@@ -4,7 +4,7 @@ export const authConfig: AuthConfig = {
 
     issuer: 'https://permataidentity.azurewebsites.net/',
     clientId: 'angularoidc',
-    postLogoutRedirectUri: 'http://localhost:4200/login-callback',
+    postLogoutRedirectUri: 'http://permata.com.br/login-callback',
     redirectUri: window.location.origin + "/login-callback",
     scope:"openid profile email",
     oidc: true,
