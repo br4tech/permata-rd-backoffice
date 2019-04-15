a5e898ed4efcbf4a439e30548df65d60ecff9a82
Guilherme Silva <guilherme.tr.silva@gmail.com>
2019-04-14 23:37:39 -0300

    Alteração na rota de login-callbak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   cliente_id : "angularoidc",
 
   base_url_login: "https://permataidentity.azurewebsites.net/",
-  base_url_postback: "http://permata.com.br/login-callback",
+  base_url_postback: "https://permata.com.br/login-callback",
 
   base_url_empresa: "https://apipermatard.azurewebsites.net/api/empresa/",
 };
