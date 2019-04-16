459002c403033400b872dea2e5f2eee69d705372
Guilherme Silva <guilherme.tr.silva@gmail.com>
2019-04-16 19:55:14 -0300

    Alteracao da rota da api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,5 @@ export const environment = {
   base_url_login: "https://permataidentity.azurewebsites.net/",
   base_url_postback: "https://permata.com.br/login-callback",
 
-  base_url_empresa: "https://apipermatard.azurewebsites.net/api/empresa/",
+  base_url_empresa: "https://api-permata.azurewebsites.net/api/empresa/",
 };
